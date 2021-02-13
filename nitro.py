6e48f964f477c57c2e51847df1b5fe518b8005d5
Benz <63103053+Bennitenni111@users.noreply.github.com>
2021-02-13 19:43:11 +0500

    Update nitro.py

--- nitro.py
+++ nitro.py
@@ -1,20 +1,33 @@
 import random, string
+from colorama import init, Fore
 import webbrowser
 
-print("Benz, Nitro Code Generator! ")
-print("Thank you for using it!\n\n\n")
+
+print(Fore.GREEN + '''
+ █████╗ ███╗   ██╗ ██████╗ ███╗   ██╗██╗██╗  ██╗
+██╔══██╗████╗  ██║██╔═══██╗████╗  ██║██║╚██╗██╔╝
+███████║██╔██╗ ██║██║   ██║██╔██╗ ██║██║ ╚███╔╝ 
+██╔══██║██║╚██╗██║██║   ██║██║╚██╗██║██║ ██╔██╗ 
+██║  ██║██║ ╚████║╚██████╔╝██║ ╚████║██║██╔╝ ██╗
+╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
+''')
+print("It will only generate nitro for you, you need checker to check them find them online.\n")
+print("We do not allow copies of this generator, we don't recommend making a video on it, we will take it down.\n")
+input("Press enter if you agree to this, program will start\n")
+
 
 num=input('Input How Many Codes to Generate: ')
 
 f=open("Nitro Codes.txt","w", encoding='utf-8')
 
-print("Wait, Generating for you!")
+print(Fore.BLUE + 'Wait, Generating for you!')
       
 for n in range(int(num)):
    y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
    f.write('https://discord.gift/')
    f.write(y)
    f.write("\n")
+       
 
-f.close()
-input("\n\nFinished, Press enter to exit. Codes saved in Nitro Codes")
+print(Fore.CYAN + 'Successfully, generated they are in Nitro Codes.txt"')           
+input("Press Enter To Close!")
