f6a43ba51b8b6c3ce586af6259f8c1e97a98a436
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-02-18 20:32:15 +0100

    Did some optimisations

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ import webbrowser
 print(Fore.GREEN + '''
  █████╗ ███╗   ██╗ ██████╗ ███╗   ██╗██╗██╗  ██╗
 ██╔══██╗████╗  ██║██╔═══██╗████╗  ██║██║╚██╗██╔╝
-███████║██╔██╗ ██║██║   ██║██╔██╗ ██║██║ ╚███╔╝ 
-██╔══██║██║╚██╗██║██║   ██║██║╚██╗██║██║ ██╔██╗ 
+███████║██╔██╗ ██║██║   ██║██╔██╗ ██║██║ ╚███╔╝
+██╔══██║██║╚██╗██║██║   ██║██║╚██╗██║██║ ██╔██╗
 ██║  ██║██║ ╚████║╚██████╔╝██║ ╚████║██║██╔╝ ██╗
 ╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝╚═╝  ╚═╝
 ''')
@@ -16,18 +16,20 @@ print("We do not allow copies of this generator, we don't recommend making a vid
 input("Press enter if you agree to this, program will start\n")
 
 
-num=input('Input How Many Codes to Generate: ')
+num = input('Input How Many Codes to Generate: ')
+charSet = f"{string.ascii_uppercase}{string.digits}{string.ascii_lowercase}"
+bigStr = ""
 
-f=open("Nitro Codes.txt","w", encoding='utf-8')
+with open("Nitro Codes.txt","w", encoding='utf-8') as file:
 
-print(Fore.BLUE + 'Wait, Generating for you!')
-      
-for n in range(int(num)):
-   y = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase) for _ in range(16))
-   f.write('https://discord.gift/')
-   f.write(y)
-   f.write("\n")
-       
+    print(f'{Fore.BLUE}Wait, Generating for you!')
 
-print(Fore.CYAN + 'Successfully, generated they are in Nitro Codes.txt"')           
-input("Press Enter To Close!")
+    for i in range(int(num)):
+        bigStr += f'https://discord.gift/{"".join(random.choices(charSet, k = 16))}\n'
+        if i % 100_000 == 0:
+            file.write(f'{bigStr}\n')
+            bigStr = ""
+
+
+    print(f'{Fore.CYAN}Successfully, generated they are in Nitro Codes.txt"')
+    input("Press Enter To Close!")
